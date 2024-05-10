59dbfb45e9ee87946dcedfe93c0d5e197c193042
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-05-10 18:39:09 +0500

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const val = (b) => {
+    document.getElementById("input_plus_past").value += b;
+}
+const solve = () => {
+    let a = document.getElementById("input_plus_past").value
+    let c = eval(a)
+    document.getElementById("input_plus_past").value = c;
+}
+const clr = () => {
+    document.getElementById("input_plus_past").value = " ";
+}
+
